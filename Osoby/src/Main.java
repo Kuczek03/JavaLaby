fae07721531b43d1b4eb4ea3752bf8262c318634
TheKolorowy <thekolorowyyt@gmail.com>
2023-04-24 11:17:08 +0200

    Lab 3 skończone

--- Osoby/src/Main.java
+++ Osoby/src/Main.java
@@ -1,9 +1,7 @@
-import java.time.LocalDate;
-
-
 public class Main {
     public static void main(String[] args){
-        Osoba osoba = new Osoba(230494100, LocalDate.of(2023,4,17),"Kowal","Jan", "Kazimierz");
-        System.out.println(osoba.toString());
+        //Osoba osoba = new Osoba(230494100, LocalDate.of(2023,4,17),"Kowal","Jan", "Kazimierz");
+        Osoba czlowiek = Osoba.wprowadz();
+        System.out.println(czlowiek.toString());
     }
 }
